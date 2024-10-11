4a0dcfaaf46891645cfda1b99741e50e2519cf21
Avishka <avishkakapuruge@gmail.com>
2024-10-11 18:13:43 +0530

    RSC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { auth } from "@/lib/auth";
 import { Separator } from '@/components/ui/separator';
 import Chat from '@/app/components/chat';
+import { Suspense } from 'react';
+import PreviousChats from './components/previous-chat';
 
 export default async function Home() {
   const session = await auth();
@@ -10,6 +12,10 @@ export default async function Home() {
       {!session?.user && <div>You need to log in to use this chat.</div>}
       {session?.user && (
         <>
+          <Suspense fallback={<div>Loading Previous Chats</div>}>
+            <PreviousChats />
+          </Suspense>
+          <h4 className="mt-5 text-2xl font-bold">New Chat Session</h4>
           <Separator className="my-5" />
           <Chat />
         </>
--- src/app/server-actions/get-completion.ts
+++ src/app/server-actions/get-completion.ts
@@ -1,11 +1,14 @@
 "use server";
 import OpenAI from "openai";
+import { auth } from "@/lib/auth";
+import { createChat, updateChat } from "@/db";
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY!,
 });
 
 export async function getCompletion(
+    id: number | null,
     messageHistory: {
         role: "user" | "assistant";
         content: string;
@@ -13,7 +16,10 @@ export async function getCompletion(
 ) {
     const response = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
-        messages: messageHistory,
+        messages: messageHistory.map((message) => ({
+            role: message.role,
+            content: message.content
+        })),
     });
 
     const messages = [
@@ -24,7 +30,20 @@ export async function getCompletion(
         },
     ];
 
+    const session = await auth();
+    let chatId = id;
+    if (!chatId) {
+        chatId = await createChat(
+            session?.user?.email!,
+            messageHistory[0].content,
+            messages
+        );
+    } else {
+        await updateChat(chatId, messages);
+    }
+
     return {
         messages,
+        id: chatId,
     };
 }
