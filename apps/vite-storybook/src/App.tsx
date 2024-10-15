7e28950b865ac1684dbb52d0d6c2ccfefbba21cc
Avishka <avishkakapuruge@gmail.com>
2024-10-15 12:26:54 +0530

    update monorepo

---
+++ apps/vite-storybook/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from "@repo/ui/button";
+
+function App() {
+
+  return (
+   <Button appName="vite-storybook">Click me</Button>
+  )
+}
+
+export default App
