4abedc42d88297944a508a10b4340c9ff782d5e8
Avishka <avishkakapuruge@gmail.com>
2024-10-16 16:53:45 +0530

    update form management

---
+++ src/app/api/register/route.tsx
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+import { schema } from "../../registration-schema";
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  let parsed = schema.safeParse(data);
+  // Add data to the database
+  if (parsed.success) {
+    // Add parsed.data to the database
+    return NextResponse.json({ message: "User registered", data: parsed.data });
+  } else {
+    return NextResponse.json({ error: parsed.error }, { status: 400 });
+  }
+}
---
+++ src/app/registration-form.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { schema } from "./registration-schema";
+
+export const RegistrationForm = ({
+  onDataAction,
+}: {
+  onDataAction: (data: any) => Promise<{
+    message: string;
+    user?: z.infer<typeof schema>;
+    issues?: string[];
+  }>;
+}) => {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      first: "",
+      last: "",
+      email: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof schema>) => {
+    // fetch("/api/register", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data));
+    // form data
+    // const formData = new FormData(); // Create a new FormData object
+    // formData.append("first", data.first);
+    // formData.append("last", data.last);
+    // formData.append("email", data.email);
+    // fetch("/api/register-form", {
+    //   method: "POST",
+    //   body: formData,
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data));
+    console.log(await onDataAction(data));
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="first"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormLabel>First Name</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormDescription>Your first name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="last"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormLabel>Last Name</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormDescription>Your last name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormDescription>Your email address.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
